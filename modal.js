93748f066b28313c60c14ebac2379c10fa6114e7
Brahim Zerd <brahim.zerd@live.fr>
2022-05-12 23:10:46 +0200

    modification du html/ ajout des label for & intégration de la préselection de l'input

--- modal.js
+++ modal.js
@@ -12,7 +12,14 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
-
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const participations = document.getElementById("quantity");
+const form = document.querySelector("form");
+var inputs = document.getElementsByTagName("input");
+const firstError = document.getElementById("firsterror");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -26,5 +33,23 @@ function close() {
 };
 // appel pour fermeture modal au click
 modalClose.addEventListener('click', close);
+
+//tentative validation formulaire
+
+form.addEventListener("submit",function e(){
+  e.preventDefault();
+  console.log("click");
+});
+
+
+
+  
+
+
+
+
+
+
+
 
 
